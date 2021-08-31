0a8d81e7cc75be1703bbef4648cca7f3a321a25d
Eduardo Barrancos <eduardobarrancos11488@gmail.com>
2021-08-31 16:58:56 +0100

    MutiplicativePersistence (NOTWORKING)

--- src/MathFunctions/MultiplicativePersistence.cpp
+++ src/MathFunctions/MultiplicativePersistence.cpp
@@ -1,5 +1,6 @@
 // Standard Library Imports
 #include <vector>
+#include <iostream>
 
 // Third Party Imports
 
@@ -11,7 +12,25 @@ using namespace std;
 
 MultiplicativePersistence::MultiplicativePersistence(Integer* number){
     //TODO: Calculate MultiplicativePersistance
-    _result = new Integer(number->GetValue() * number->GetValue());
+    Integer auxNum;
+    number->CloneInteger(&auxNum);
+    _result = new Integer(0);
+
+    this->_AuxMultiplicativePersistence(auxNum, _result);
+    cout << _result->GetValue();
+}
+
+void MultiplicativePersistence::_AuxMultiplicativePersistence(Integer num, Integer* mult){
+    cout << "LEz go\n";
+    cout << num.GetNumberOfDigits() << "\n";
+
+    if(num.GetNumberOfDigits() >= 1){
+        num.SetValue(num.GetProductOfDigits());
+        mult->SetValue(mult->GetValue()+1);
+        cout << mult->GetValue();
+        _AuxMultiplicativePersistence(num,mult);
+    }
+    return;
 }
 
 vector<string> MultiplicativePersistence::s_names = {"multiplicative_persistence"};
