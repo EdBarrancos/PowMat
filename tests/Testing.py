b2c314c19a7333844f58b0ec52741278e4e11095
Eduardo Barrancos <eduardobarrancos11488@gmail.com>
2021-09-26 13:07:18 +0100

    Start New TEsting

---
+++ tests/Testing.py
@@ -0,0 +1,17 @@
+import filecmp
+import subprocess
+import os
+from glob import glob
+
+
+HEADER = '\033[95m'
+OKBLUE = '\033[94m'
+OKCYAN = '\033[96m'
+OKGREEN = '\033[92m'
+WARNING = '\033[93m'
+FAIL = '\033[91m'
+ENDC = '\033[0m'
+BOLD = '\033[1m'
+UNDERLINE = '\033[4m'
+
+FILES = glob("inputs/*")
